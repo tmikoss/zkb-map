a84ca58d78fb2cbc9276b4a962e32796d78cef53
Toms Mikoss <toms.mikoss@gmail.com>
2020-08-17 22:16:35 +0300

    Data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useKillmails } from './useKillmails'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const sourceUrl = 'wss://zkillboard.com/websocket/'
+  const killmails = useKillmails({ sourceUrl })
+
+  return <pre>
+    {JSON.stringify(killmails, null, 2)}
+  </pre>
 }
 
 export default App;
---
+++ src/useKillmails.tsx
@@ -0,0 +1,120 @@
+import { useEffect, useReducer } from 'react'
+import parseISO from 'date-fns/parseISO'
+import differenceInSeconds from 'date-fns/differenceInSeconds'
+
+const SUBSCRIBE_MESSAGE = JSON.stringify({
+  "action": "sub",
+  "channel": "killstream"
+})
+
+const MAX_KILLMAIL_AGE_SEC = 60
+const DECAY_INTERVAL_MS = 10 * 1000
+
+type UnparsedTimestamp = string
+
+interface WebsocketKillmail {
+  killmail_id: number
+  killmail_time: UnparsedTimestamp
+  solar_system_id: number
+  victim: {
+    alliance_id?: number
+    character_id: number
+    corporation_id?: number
+    ship_type_id: number
+    position: {
+      x: number,
+      y: number,
+      z: number
+    }
+  }
+  zkb: {
+    totalValue: number
+    fittedValue: number
+    locationID: number
+    npc: boolean
+    awox: boolean
+    solo: boolean
+    url: string
+  }
+}
+
+interface Killmail {
+  id: number
+  time: Date
+  receivedAt: Date
+  characterId: number
+  corporationId?: number
+  allianceId?: number
+  shipTypeId: number
+  solarSystemId: number
+  url: string
+}
+
+type ReceiveWebsocketAction = {
+  type: 'RECEIVE_WEBSOCKET'
+  payload: WebsocketKillmail
+}
+
+type DecayAction = {
+  type: 'DECAY'
+}
+
+type KillmailAction = ReceiveWebsocketAction | DecayAction
+
+function reduceKillmails(state: Killmail[], action: KillmailAction): Killmail[] {
+  console.log(action)
+  switch (action.type) {
+    case 'RECEIVE_WEBSOCKET': {
+      const { killmail_id, killmail_time, victim, solar_system_id, zkb } = action.payload
+
+      const time = parseISO(killmail_time)
+
+      const killmail: Killmail = {
+        id: killmail_id,
+        time,
+        receivedAt: new Date(),
+        characterId: victim.character_id,
+        corporationId: victim.corporation_id,
+        allianceId: victim.alliance_id,
+        shipTypeId: victim.ship_type_id,
+        solarSystemId: solar_system_id,
+        url: zkb.url
+      }
+
+      return [...state, killmail]
+    }
+    case 'DECAY': {
+      const now = new Date()
+      return state.filter(killmail => differenceInSeconds(now, killmail.receivedAt) < MAX_KILLMAIL_AGE_SEC)
+    }
+    default:
+      return state
+  }
+}
+
+export function useKillmails(props: { sourceUrl: string }): Killmail[] {
+  const { sourceUrl } = props
+  const [killmails, dispatch] = useReducer(reduceKillmails, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => dispatch({ type: 'DECAY' }), DECAY_INTERVAL_MS)
+    return () => clearInterval(interval)
+  }, [dispatch])
+
+  useEffect(() => {
+    const connection = new WebSocket(sourceUrl)
+
+    connection.onopen = () => {
+      // connection.send(SUBSCRIBE_MESSAGE)
+    }
+
+    connection.onmessage = (e) => {
+      const killmail: WebsocketKillmail = JSON.parse(e.data)
+      dispatch({ type: 'RECEIVE_WEBSOCKET', payload: killmail })
+    }
+
+    return () => connection.close()
+  }, [sourceUrl, dispatch])
+
+  return killmails
+}
