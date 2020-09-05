0fb64b9d6c89461a24b2ab9aac509c7be0fd6f83
Toms Mikoss <toms.mikoss@gmail.com>
2020-09-05 12:34:39 +0300

    Updates

---
+++ precache-manifest.2c685c3af6f430b179628735509027a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ba8bf5adda3be9048ec8b038ae39c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2606baf280f7736e07e7",
+    "url": "/static/js/2.5d1faa06.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/static/js/2.5d1faa06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78126e69b52de4e3909c",
+    "url": "/static/js/main.15071f7a.chunk.js"
+  },
+  {
+    "revision": "ac95874cbe1fb3f41323",
+    "url": "/static/js/runtime-main.36485399.js"
+  },
+  {
+    "revision": "3b6b0c759a85e6ff915f53fed58e066d",
+    "url": "/static/media/glow.3b6b0c75.png"
+  }
+]);
