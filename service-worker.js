16e8bbd048b3bf2f322ae2e1908057b04c060e93
Toms Mikoss <toms.mikoss@gmail.com>
2020-09-07 22:49:51 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://tmikoss.github.io/zkb-map/precache-manifest.158865643f0dd1bd32d9e61760706c3c.js"
+  "https://tmikoss.github.io/zkb-map/precache-manifest.5017c2c784c9bf20ba7785eb9be37a08.js"
 );
 
 self.addEventListener('message', (event) => {
