f42ffa4d45a55fc60946b5b4bb2fc66c4d60a46e
Toms Mikoss <toms.mikoss@gmail.com>
2020-09-13 19:29:54 +0300

    Better chase camera

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -5,7 +5,8 @@ import * as THREE from 'three'
 import { CameraMode } from './store/configuration'
 import { positionToArray, HasPosition } from './utils/geometry'
 import { useFrame } from 'react-three-fiber'
-import map from 'lodash/map'
+import { ageMultiplier } from './utils/scaling'
+import differenceInMilliseconds from 'date-fns/differenceInMilliseconds'
 
 const near = 0.001
 const far = 10_000
@@ -14,8 +15,8 @@ const fov = 90
 const defaultPosition = new THREE.Vector3(0, 0, 700)
 const defaultQuaternion = new THREE.Quaternion()
 
-const minZ = defaultPosition.z / 3
-const movementMultiplier = 0.02
+const minRadius = defaultPosition.z / 3
+const movementMultiplier = 0.01
 
 const lookAtPoints = (points: HasPosition[]): THREE.Vector3 => {
   const count = points.length
@@ -34,7 +35,7 @@ const lookAtPoints = (points: HasPosition[]): THREE.Vector3 => {
   if (geometry.boundingSphere) {
     const { center, radius } = geometry.boundingSphere
 
-    center.z += Math.max(radius * 1.1, minZ)
+    center.z += Math.max(radius * 1.1, minRadius)
     return center
   } else {
     return defaultPosition
@@ -53,6 +54,9 @@ const Camera: React.FC<{
     const solarSystemArray = Object.values(solarSystems)
     if (solarSystemArray.length > 0) {
       position.current = lookAtPoints(solarSystemArray)
+      if (ref.current) {
+        ref.current.position.lerp(position.current, 1)
+      }
     }
   }, [solarSystems])
 
@@ -60,8 +64,70 @@ const Camera: React.FC<{
     if (ref.current && position.current && mode === CameraMode.follow) {
       let target = position.current
 
-      if (killmails.current && killmails.current.length > 0) {
-        target = lookAtPoints(map(killmails.current, km => solarSystems[km.solarSystemId]))
+      const count = killmails.current?.length || 0
+
+      if (count > 0) {
+        const now = new Date()
+
+        let maxX = -Infinity
+        let maxY = -Infinity
+        let maxZ = -Infinity
+
+        let minX = +Infinity
+        let minY = +Infinity
+        let minZ = +Infinity
+
+        let totalX = 0
+        let totalY = 0
+        let totalZ = 0
+
+        let totalScale = 0
+
+        for (let index = 0; index < count; index++) {
+          const { solarSystemId, scaledValue, receivedAt } = killmails.current[index]
+          const { x, y, z } = solarSystems[solarSystemId]
+
+          const age = differenceInMilliseconds(now, receivedAt)
+          const scale = scaledValue * ageMultiplier(age, scaledValue)
+
+          if (scale > 0.1) {
+            const scaledX = scale * x
+            const scaledY = scale * y
+            const scaledZ = scale * z
+
+            maxX = Math.max(maxX, x)
+            maxY = Math.max(maxY, y)
+            maxZ = Math.max(maxZ, z)
+
+            minX = Math.min(minX, x)
+            minY = Math.min(minY, y)
+            minZ = Math.min(minZ, z)
+
+            totalX += scaledX
+            totalY += scaledY
+            totalZ += scaledZ
+
+            totalScale += scale
+          }
+        }
+
+        if (totalScale > 0) {
+          const x = totalX / totalScale
+          const y = totalY / totalScale
+          const z = totalZ / totalScale
+
+          const radius = Math.max(
+            maxX - x,
+            maxY - y,
+            maxZ - z,
+            x - minX,
+            y - minY,
+            z - minZ,
+            minRadius
+          )
+
+          target = new THREE.Vector3(x, y, z + radius * 1.1)
+        }
       }
 
       ref.current.position.lerp(target, movementMultiplier)
