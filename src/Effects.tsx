32f633ffa29b59e0205d642683f2a62dfa97d59b
Toms Mikoss <toms.mikoss@gmail.com>
2020-09-13 21:42:08 +0300

    Turrn off multisampling for mobile support

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { EffectComposer, Bloom, Noise, Vignette } from 'react-postprocessing'
 
+const doesNotWorkOnAndroidBrowsersAndShouldBeFeatureDetected = 0
+
 const Effects: React.FC = () => {
-  return <EffectComposer>
+  return <EffectComposer multisampling={doesNotWorkOnAndroidBrowsersAndShouldBeFeatureDetected}>
     <Bloom luminanceThreshold={0.2} luminanceSmoothing={0.9} intensity={3} />
     <Noise opacity={0.04} />
     <Vignette eskil={false} offset={0.1} darkness={1.1} />
