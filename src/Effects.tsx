5485a0eccd77b60d7d183b4e164ba7299fb01f8c
Toms Mikoss <toms.mikoss@gmail.com>
2020-09-14 22:51:52 +0300

    Disable multisampling on low power devices

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { memo} from 'react'
 import { EffectComposer, Bloom, Noise, Vignette } from 'react-postprocessing'
-
-const doesNotWorkOnAndroidBrowsersAndShouldBeFeatureDetected = 0
+import { useThree } from 'react-three-fiber'
 
 const Effects: React.FC = () => {
-  return <EffectComposer multisampling={doesNotWorkOnAndroidBrowsersAndShouldBeFeatureDetected}>
+  const { gl } = useThree()
+  // Multisampling kills WebGL renderer on low power devices. This seems to be reasonable way of detection.
+  const multisampling = gl.capabilities.maxTextureSize > 8192 ? 8 : 0
+
+  return <EffectComposer multisampling={multisampling}>
     <Bloom luminanceThreshold={0.2} luminanceSmoothing={0.9} intensity={3} />
     <Noise opacity={0.04} />
     <Vignette eskil={false} offset={0.1} darkness={1.1} />
   </EffectComposer>
 }
 
-export default Effects
+export default memo(Effects)
