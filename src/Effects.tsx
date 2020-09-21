db5802cdc329909319c6414542f74ead2ca3acad
Toms Mikoss <toms.mikoss@gmail.com>
2020-09-21 11:57:11 +0300

    SMAA needs suspense

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,4 +1,4 @@
-import React, { memo} from 'react'
+import React, { memo, Suspense} from 'react'
 import { EffectComposer, Bloom, Noise, Vignette, SMAA } from 'react-postprocessing'
 import { useThree } from 'react-three-fiber'
 import compact from 'lodash/compact'
@@ -15,7 +15,9 @@ const Effects: React.FC = () => {
     !enableMultisampling && <SMAA key='smaa' />
   ])
 
-  return <EffectComposer multisampling={enableMultisampling ? 8 : 0}>{effects}</EffectComposer>
+  return <Suspense fallback={null}>
+    <EffectComposer multisampling={enableMultisampling ? 8 : 0}>{effects}</EffectComposer>
+  </Suspense>
 }
 
 export default memo(Effects)
