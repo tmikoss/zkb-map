635fd4c80ab665ca094af12a41752f460d2898ee
Toms Mikoss <toms.mikoss@gmail.com>
2020-10-20 21:12:47 +0300

    Use GPU tier detection to differentiate effects

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,15 +1,20 @@
 import React, { memo, Suspense} from 'react'
 import { EffectComposer, Bloom, Noise, Vignette } from '@react-three/postprocessing'
-import { useThree } from 'react-three-fiber'
 import compact from 'lodash/compact'
+import { useDetectGPU } from '@react-three/drei/useDetectGPU'
 
 const Effects: React.FC = () => {
-  const { gl } = useThree()
-  const { isWebGL2 } = gl.capabilities
+  const { tier } = useDetectGPU()
+
+  if (tier === "0") {
+    return null
+  }
+
+  const adequateGpu = parseInt(tier) > 1
 
   const effects = compact([
-    isWebGL2 && <Bloom luminanceThreshold={0.4} luminanceSmoothing={1} intensity={2} key='bloom' />,
-    isWebGL2 && <Noise opacity={0.04} key='noise' />,
+    adequateGpu && <Bloom luminanceThreshold={0.4} luminanceSmoothing={1} intensity={2} key='bloom' />,
+    adequateGpu && <Noise opacity={0.04} key='noise' />,
     <Vignette eskil={false} offset={0.1} darkness={1.1} key='vignette' />
   ])
 
