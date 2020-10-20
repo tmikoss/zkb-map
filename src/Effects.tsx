8b61e992543f417c625e9dc6cd7a92a5752ced6b
Toms Mikoss <toms.mikoss@gmail.com>
2020-10-20 19:41:23 +0300

    Upgrade postporcessing + three

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,22 +1,20 @@
 import React, { memo, Suspense} from 'react'
-import { EffectComposer, Bloom, Noise, Vignette, SMAA } from 'react-postprocessing'
+import { EffectComposer, Bloom, Noise, Vignette } from '@react-three/postprocessing'
 import { useThree } from 'react-three-fiber'
 import compact from 'lodash/compact'
 
 const Effects: React.FC = () => {
   const { gl } = useThree()
-  const { isWebGL2, maxTextureSize } = gl.capabilities
-  const enableMultisampling = isWebGL2 && maxTextureSize > 8192
+  const { isWebGL2 } = gl.capabilities
 
   const effects = compact([
     isWebGL2 && <Bloom luminanceThreshold={0.4} luminanceSmoothing={1} intensity={2} key='bloom' />,
     isWebGL2 && <Noise opacity={0.04} key='noise' />,
-    <Vignette eskil={false} offset={0.1} darkness={1.1} key='vignette' />,
-    !enableMultisampling && <SMAA key='smaa' />
+    <Vignette eskil={false} offset={0.1} darkness={1.1} key='vignette' />
   ])
 
   return <Suspense fallback={null}>
-    <EffectComposer multisampling={enableMultisampling ? 8 : 0}>{effects}</EffectComposer>
+    <EffectComposer>{effects}</EffectComposer>
   </Suspense>
 }
 
